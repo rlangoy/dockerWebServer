34741d85302441d9d0b539dce1a70993cf07f8bc
Rune Langøy <rune.langoy@gmail.com>
2025-08-20 14:01:35 +0200

    Switch to spdlog to avoild linker issues whth boost__log
    linking boost__log in windows and conan is not stright forward.
    but ok in Docker / Ubuntu

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,20 @@
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <spdlog/spdlog.h>
+
 #include <iostream>
 #include <string>
 
-namespace beast = boost::beast;     // Boost.Beast
-namespace http = beast::http;       // HTTP
-namespace net = boost::asio;        // Networking
+namespace beast = boost::beast;
+namespace http = beast::http;
+namespace net = boost::asio;
 using tcp = net::ip::tcp;
 
 int main() {
+    // Set log level (debug, info, warn, error, critical, off)
+    spdlog::set_level(spdlog::level::warn);
+
     try {
         net::io_context ioc;
 
@@ -26,20 +31,21 @@ int main() {
             http::request<http::string_body> req;
             http::read(socket, buffer, req);
 
-            std::cout << "Request: " << req << "\n";
-
+            // Clean, simple logging
+            spdlog::info("Request: {} {}", req.method_string(), req.target());
+            
             // Make response
             http::response<http::string_body> res{
                 http::status::ok, req.version()};
             res.set(http::field::server, "Beast");
             res.set(http::field::content_type, "text/html");
-            res.body() = "<h1>Hello World </h1>";  
+            res.body() = "<h1>Hello World</h1>";  
             res.prepare_payload();
 
             http::write(socket, res);
         }
-    } catch (std::exception const& e) {
-        std::cerr << "Error: " << e.what() << "\n";
+    } catch (std::exception const& e) {        
+        spdlog::error("Error: {}", e.what());
         return 1;
     }
 }
