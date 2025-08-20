4a3b9d875f0c1265c80d5f061327e43ecf786ddb
Rune Langøy <rune.langoy@gmail.com>
2025-08-20 09:09:02 +0200

    Switch to Boost / Beast for readability

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,45 @@
-#include <fmt/core.h>
-#include <fmt/color.h>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
+#include <boost/asio/ip/tcp.hpp>
 #include <iostream>
 #include <string>
-#include <cstring>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
 
- 
+namespace beast = boost::beast;     // Boost.Beast
+namespace http = beast::http;       // HTTP
+namespace net = boost::asio;        // Networking
+using tcp = net::ip::tcp;
 
 int main() {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (server_fd == 0) {
-        perror("socket failed");
-        return 1;
-    }
+    try {
+        net::io_context ioc;
 
-    sockaddr_in address;
-    int opt = 1;
-    int addrlen = sizeof(address);
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
+        // Listen on 0.0.0.0:8080
+        tcp::acceptor acceptor{ioc, {tcp::v4(), 8080}};
+        std::cout << "Server running at http://localhost:8080\n";
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        return 1;
-    }
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        return 1;
-    }
+        for (;;) {
+            tcp::socket socket{ioc};
+            acceptor.accept(socket);
 
-    fmt::print("Server running on port {} ... !\n", 8080);
+            beast::flat_buffer buffer;
+            http::request<http::string_body> req;
+            http::read(socket, buffer, req);
 
-    while (true) {
-        int new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
-        if (new_socket < 0) {
-            perror("accept");
-            continue;
+            std::cout << "Request: " << req << "\n";
+
+            // Make response
+            http::response<http::string_body> res{
+                http::status::ok, req.version()};
+            res.set(http::field::server, "Beast");
+            res.set(http::field::content_type, "text/html");
+            res.body() = "<h1>Hello from Boost.Beast 🚀</h1>";
+            res.prepare_payload();
+
+            http::write(socket, res);
         }
-        std::string response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello from C++ webserver!";
-        send(new_socket, response.c_str(), response.size(), 0);
-        close(new_socket);
+    } catch (std::exception const& e) {
+        std::cerr << "Error: " << e.what() << "\n";
+        return 1;
     }
-
-    return 0;
 }
